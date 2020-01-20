d8ca954416fad1d67b4a5403320f2f5874eb6a3c
woodpecker <tomdziwood@gmail.com>
2020-01-20 17:49:56 +0100

    Import service upgrade
    - add basic type chcecking of data file
    - returning attributes and objects in json format after importing of metadata and data

--- src/main/java/pl/put/poznan/rulework/rest/ImportController.java
+++ src/main/java/pl/put/poznan/rulework/rest/ImportController.java
@@ -3,6 +3,8 @@ package pl.put.poznan.rulework.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.put.poznan.rulework.service.ImportService;
@@ -23,13 +25,13 @@ public class ImportController {
         this.importService = importService;
     }
 
-    @PostMapping
-    public String uploadFile(
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uploadFile(
             @RequestParam("metadata") MultipartFile metadataFile,
-            @RequestParam("data") MultipartFile dataFile,
-            @RequestParam("format") String format) throws IOException {
+            @RequestParam("data") MultipartFile dataFile) throws IOException {
 
         logger.info("Importing data");
-        return importService.importData(metadataFile, dataFile, format);
+        String result = importService.importData(metadataFile, dataFile);
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/pl/put/poznan/rulework/service/ImportService.java
+++ src/main/java/pl/put/poznan/rulework/service/ImportService.java
@@ -1,8 +1,12 @@
 package pl.put.poznan.rulework.service;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.rulelearn.data.*;
 import org.rulelearn.data.csv.ObjectParser;
 import org.rulelearn.data.json.AttributeParser;
+import org.rulelearn.data.json.InformationTableWriter;
 import org.rulelearn.types.EvaluationField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +14,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ImportService {
@@ -18,11 +24,10 @@ public class ImportService {
 
     public static InformationTable informationTable;
 
-    public String importData(MultipartFile metadataFile, MultipartFile dataFile, String format) throws IOException {
+    public String importData(MultipartFile metadataFile, MultipartFile dataFile) throws IOException {
 
         logger.info("Metadata:\t" + metadataFile.getOriginalFilename() + "\t" + metadataFile.getContentType());
         logger.info("Data:\t" + dataFile.getOriginalFilename() + "\t" + dataFile.getContentType());
-        logger.info("Data's format: " + format);
 
         Attribute[] attributes;
         AttributeParser attributeParser = new AttributeParser();
@@ -32,9 +37,22 @@ public class ImportService {
             logger.info(i + ":\t" + attributes[i]);
         }
 
-        ObjectParser objectParser = new ObjectParser.Builder(attributes).header(true).build();
-        reader = new InputStreamReader(dataFile.getInputStream());
-        informationTable = objectParser.parseObjects(reader);
+        if (dataFile.getContentType().equals("application/json")) {
+            logger.info("Data type is json");
+            org.rulelearn.data.json.ObjectParser objectParser = new org.rulelearn.data.json.ObjectParser.Builder(attributes).build();
+            reader = new InputStreamReader(dataFile.getInputStream());
+            informationTable = objectParser.parseObjects(reader);
+
+        } else if (dataFile.getContentType().equals("application/vnd.ms-excel")) {
+            logger.info("Data type is csv");
+            ObjectParser objectParser = new ObjectParser.Builder(attributes).build();
+            reader = new InputStreamReader(dataFile.getInputStream());
+            informationTable = objectParser.parseObjects(reader);
+        } else {
+            logger.error("Unrecognized format of data file: " + dataFile.getContentType());
+        }
+
+
         Table<EvaluationAttribute, EvaluationField> table = informationTable.getActiveConditionAttributeFields();
         for(int i = 0; i < table.getNumberOfObjects(); i++) {
             StringBuilder sb = new StringBuilder();
@@ -47,7 +65,27 @@ public class ImportService {
             logger.info(sb.toString());
         }
 
-        return "End of uploadFile (POST)";
+        StringWriter attributesWriter = new StringWriter();
+        StringWriter objectsWriter = new StringWriter();
+        InformationTableWriter itw = new InformationTableWriter(true);
+        itw.writeAttributes(informationTable, attributesWriter);
+        itw.writeObjects(informationTable, objectsWriter);
+        logger.info("attributesWriter:\n" + attributesWriter);
+        logger.info("objectsWriter:\n" + objectsWriter);
+
+        List<String> array = new ArrayList<>();
+        array.add(attributesWriter.toString());
+        array.add(objectsWriter.toString());
+
+        /*JsonArray attributesJson = (JsonArray) new JsonParser().parse(attributesWriter.toString());
+        JsonArray objectsJson = (JsonArray) new JsonParser().parse(objectsWriter.toString());
+        JsonArray jsonArray = new JsonArray();
+        jsonArray.add(attributesJson);
+        jsonArray.add(objectsJson);*/
+
+
+
+        return array.toString();
     }
 
 }
