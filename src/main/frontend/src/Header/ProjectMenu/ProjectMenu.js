bce1c080dfdb02fc55b1201bb5fa924f023a9dfc
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-01-24 10:54:19 +0100

    Change ProjectMenu from hook to class

--- src/main/frontend/src/Header/ProjectMenu/ProjectMenu.js
+++ src/main/frontend/src/Header/ProjectMenu/ProjectMenu.js
@@ -1,54 +1,74 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 
-function ProjectMenu(props) {
-    const [anchorE1, setAnchorE1] = useState(null);
-    const [selectedIndex, setSelectedIndex] = useState(props.currentProject);
+class ProjectMenu extends Component {
+    constructor(props) {
+        super(props);
 
-    const handleClickListItem = event => {
-        setAnchorE1(event.currentTarget);
+        this.state = {
+            anchorE1: null,
+            selectedIndex: this.props.currentProject,
+        };
+
+        this.handleClickListItem = this.handleClickListItem.bind(this);
+        this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleClickListItem(event) {
+        this.setState({
+            anchorE1: event.currentTarget,
+        });
     };
 
-    const handleMenuItemClick = (event, index) => {
-        setSelectedIndex(index);
-        setAnchorE1(null);
+    handleMenuItemClick(event, index) {
+        this.setState({
+            selectedIndex: index,
+            anchorE1: null,
+        });
     };
 
-    const handleClose = () => {
-        setAnchorE1(null);
+    handleClose() {
+        this.setState({
+            anchorE1: null,
+        });
     };
 
-    return (
-        <div>
-            <List component={"nav"} >
-                <ListItem
-                    button
-                    aria-haspopup={"true"}
-                    aria-controls={"lock-menu"}
-                    onClick={handleClickListItem}>
-                    <ListItemText primary={"Active project: "} secondary={props.projects[selectedIndex]} />
-                </ListItem>
-            </List>
-            <Menu
-                anchorEl={anchorE1}
-                keepMounted
-                open={Boolean(anchorE1)}
-                onClose={handleClose}>
-                {props.projects.map((option, index) => (
-                    <MenuItem
-                        key={option}
-                        selected={index === selectedIndex}
-                        onClick={event => handleMenuItemClick(event, index)}>
-                        {option}
-                    </MenuItem>
-                ))}
-            </Menu>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <List component={"nav"} >
+                    <ListItem
+                        button
+                        aria-haspopup={"true"}
+                        aria-controls={"lock-menu"}
+                        onClick={this.handleClickListItem}>
+                        <ListItemText
+                            primary={"Active project: "}
+                            secondary={this.props.projects[this.state.selectedIndex]} />
+                    </ListItem>
+                </List>
+                <Menu
+                    anchorEl={this.state.anchorE1}
+                    keepMounted
+                    open={Boolean(this.state.anchorE1)}
+                    onClose={this.handleClose}>
+                    {this.props.projects.map((option, index) => (
+                        <MenuItem
+                            key={option}
+                            selected={index === this.state.selectedIndex}
+                            onClick={event => this.handleMenuItemClick(event, index)}>
+                            {option}
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </div>
+        )
+    }
 }
 
 export default ProjectMenu;
