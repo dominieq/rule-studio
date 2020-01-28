3d1aa9f0ba84e064f7a82bc52aff2f5d02d1783e
emsi-iggy <michal.zimny.pl@gmail.com>
2020-01-28 21:45:31 +0100

    Right clicked context menu
    To the data grid was added new feature - right clicked context menu. It allows to add and delete rows.

--- src/main/frontend/src/Body/Data/DisplayData.js
+++ src/main/frontend/src/Body/Data/DisplayData.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDataGrid from 'react-data-grid';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Editors, Toolbar, Data} from 'react-data-grid-addons';
+import { Editors, Toolbar, Data, Menu} from 'react-data-grid-addons';
 import data from './data-example.json';
 import metadata from './metadata-example.json';
 import './DisplayData.css';
@@ -9,12 +9,13 @@ import './DisplayData.css';
 const selectors = Data.Selectors;
 
 const { DropDownEditor } = Editors;
+const { ContextMenu, MenuItem, ContextMenuTrigger } = Menu;
 
-let kolumny = [{key: "uniqueLP", name: "LP", sortable:true, resizable:true, filterable:true, width:100, visible: true}]
+let kolumny = [{key: "uniqueLP", name: "LP", sortable:true, resizable:true, filterable:true, sortDescendingFirst:true,  width:60, visible: true}]
 
 for(var el in metadata) {
     if(metadata[el].domain) {
-        kolumny.push({editable:true, sortable:true, resizable:true, filterable:true, 
+        kolumny.push({editable:true, sortable:true, resizable:true, filterable:true,
             key : metadata[el].name, name : (metadata[el].name + (metadata[el].preferenceType === "gain" ? "(+)" : "(-)")),
             editor : <DropDownEditor options={metadata[el].domain} />})
     } else {
@@ -27,12 +28,37 @@ let wiersze = [...data]
 let uniqueLP = 1
 wiersze.forEach(x => { x.uniqueLP = uniqueLP++ })
 
+
+function ExampleContextMenu({
+    idx,
+    uniqueLP,
+    rowIdx,
+    onRowDelete,
+    onRowInsertAbove,
+    onRowInsertBelow
+  }) {
+    return (
+      <ContextMenu uniqueLP={uniqueLP}>
+        <MenuItem data={{ rowIdx, idx }} onClick={onRowDelete}>
+          Delete Example
+        </MenuItem>
+        <MenuItem data={{ rowIdx, idx }} onClick={onRowInsertAbove}>
+          Add new example above
+        </MenuItem>
+        <MenuItem data={{ rowIdx, idx }} onClick={onRowInsertBelow}>
+         Add new example below
+        </MenuItem>
+      </ContextMenu>
+    );
+  }
+
 class DisplayData extends React.Component {
     constructor(props) {
         super(props);
     
         this.state = {
             rows : wiersze,
+            enableRowInsert: 0, //-1 no sort, 0-sort asc, 1-sort desc
             columns : kolumny,
             selectedRows : [],
             filters : {}
@@ -51,22 +77,30 @@ class DisplayData extends React.Component {
                 const rows_index = rows.map( x => x.uniqueLP ).indexOf(filtered[i].uniqueLP);
                 rows[rows_index] = { ...filtered[i], ...updated };
             }
-            return { rows };
+            return { 
+                rows: rows, 
+            };
         });
     };
 
     onGridSort = (sortColumn, sortDirection) => {
+        let tmpEnableRowInsert = -1
         const comparer = (a, b) => {
             if (sortDirection === "ASC") {
+                ((sortColumn === "uniqueLP") ? tmpEnableRowInsert = 0 : tmpEnableRowInsert = -1)
                 return a[sortColumn] > b[sortColumn] ? 1 : -1;
             } else if (sortDirection === "DESC") {
+                ((sortColumn === "uniqueLP") ? tmpEnableRowInsert = 1 : tmpEnableRowInsert = -1)
                 return a[sortColumn] < b[sortColumn] ? 1 : -1;
+            } else {
+                tmpEnableRowInsert = 0;
+                return a["uniqueLP"] > b["uniqueLP"] ? 1 : -1;
             }
         };
         this.setState( (prevState) => ({
-            rows: sortDirection === "NONE" ? wiersze : [...prevState.rows].sort(comparer)
+            rows: [...prevState.rows].sort(comparer),
+            enableRowInsert: tmpEnableRowInsert
         }));
-        wiersze = [...this.state.rows]
     };
 
     onRowsSelected = rows => {
@@ -113,24 +147,60 @@ class DisplayData extends React.Component {
         })
     }
 
-    handleAddRow = ({ newRowIndex }) => {
-        const newRow = {
-            value: newRowIndex,
-            id: this.state.rows.length+1,
-        };
-
-        let rows = [...this.state.rows]
-        rows = [...rows, newRow]
-        this.setState({ rows });
-    };
-
-    handleAddRow1 = () => {
-        const newRow = {
-            value: uniqueLP,
-            uniqueLP: uniqueLP++,
-        };
-
-        this.setState( prevState => ({ rows: [...prevState.rows, newRow] }));
+    deleteRow = (rowIdx) => {
+        this.setState(prevState => {
+            const nextRows = [...prevState.rows];
+            nextRows.splice(rowIdx, 1);
+            return {rows: nextRows};
+        })
+        
+      };
+    
+    insertRow = (rowIdx, where) => {       
+        this.setState(prevState => {
+            const nextRows = [...prevState.rows];
+            const newRow = {};
+            switch(where) {
+                case "above": //above the chosen row
+                    if(this.state.enableRowInsert === 0) { //sort-asc
+                        newRow.uniqueLP = nextRows[rowIdx].uniqueLP;
+                        nextRows.forEach(r => {
+                            if(r.uniqueLP >= newRow.uniqueLP) r.uniqueLP+=1
+                        });
+                        nextRows.splice(rowIdx, 0, newRow);
+                    } else if(this.state.enableRowInsert === 1) { //sort-desc
+                        newRow.uniqueLP = nextRows[rowIdx].uniqueLP+1;
+                        nextRows.forEach(r => {
+                            if(r.uniqueLP >= newRow.uniqueLP) r.uniqueLP+=1
+                        });
+                        nextRows.splice(rowIdx, 0, newRow);
+                    }
+                break;
+                case "below": //below the chosen row
+                    if(this.state.enableRowInsert === 0) { //sort-asc
+                        newRow.uniqueLP = nextRows[rowIdx].uniqueLP+1;
+                        nextRows.forEach(r => {
+                            if(r.uniqueLP >= newRow.uniqueLP) r.uniqueLP+=1
+                        });
+                        nextRows.splice(rowIdx+1, 0, newRow);
+                    } else if(this.state.enableRowInsert === 1) { //sort-desc
+                        newRow.uniqueLP = nextRows[rowIdx].uniqueLP;
+                        nextRows.forEach(r => {
+                            if(r.uniqueLP >= newRow.uniqueLP) r.uniqueLP+=1
+                        });
+                        nextRows.splice(rowIdx+1, 0, newRow);
+                    }
+                break;
+                default: //at the end of rows array
+                    newRow.uniqueLP = nextRows.length+1;
+                    nextRows.push(newRow);
+                break;
+            };
+            return { 
+                rows: nextRows
+            };
+        });
+    
     };
 
     render() {
@@ -151,7 +221,7 @@ class DisplayData extends React.Component {
                     onGridSort = {this.onGridSort}
                     enableCellSelect={true}
 
-                    toolbar={<Toolbar enableFilter={true} filterRowsButtonText={"Filter"} onAddRow={this.handleAddRow1}/> }
+                    toolbar={<Toolbar enableFilter={true} filterRowsButtonText={"Filter"} onAddRow={(a,b) => this.insertRow(0,0)}/> }
                     onAddFilter={this.handleFilterChange}
                     onClearFilters={this.onClearFilters}
 
@@ -171,6 +241,15 @@ class DisplayData extends React.Component {
                     minHeight={600}
                     rowHeight={50}
                     rowScrollTimeout={200}
+
+                    contextMenu={
+                        <ExampleContextMenu
+                          onRowDelete={(e, { rowIdx }) => this.deleteRow(rowIdx)}
+                          onRowInsertAbove={(e, { rowIdx }) => this.insertRow(rowIdx, "above")}
+                          onRowInsertBelow={(e, { rowIdx }) => this.insertRow(rowIdx, "below")}
+                        />
+                      }
+                      RowsContainer={ContextMenuTrigger}
                 />
             </div>
         )
