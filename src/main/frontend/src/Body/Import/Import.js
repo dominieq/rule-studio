75f60757a3c6e6cf34dd45b54879edaf5abd9dbc
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-02-12 10:56:50 +0100

    Delete tabs from import

--- src/main/frontend/src/Body/Import/Import.js
+++ src/main/frontend/src/Body/Import/Import.js
@@ -1,88 +1,23 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import Paper from "@material-ui/core/Paper";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
 import Box from "@material-ui/core/Box";
-import TabBody from "./TabBody";
 import "./Import.css";
-
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <Box
-            component={"div"}
-            className={"tab-body-wrapper"}
-            role={"tabpanel"}
-            hidden={value !== index}
-            id={`import-tabpanel-${index}`}
-            aria-labelledby={`import-tab-${index}`}
-            {...other}
-            >
-            {value === index && children}
-        </Box>
-    )
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-};
 
 class Import extends Component{
     constructor(props) {
         super(props);
 
         this.state = {
-            value: 0,
+            chosenName: "new project",
+            chosenFiles: [],
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.setTabProps = this.setTabProps.bind(this);
-    }
-
-    handleChange(event, newValue) {
-        this.setState({
-            value: newValue,
-        });
-    };
-
-    setTabProps(index) {
-        return ({
-            id: `import-tab-${index}`,
-            'aria-controls': `import-tabpanel-${index}`,
-        });
     }
 
     render() {
         return (
             <Box component={"div"} className={"import-root"}>
-                <Paper square={true} className={"tabs-wrapper"}>
-                    <Paper elevation={3}>
-                        <Tabs
-                            value={this.state.value}
-                            onChange={this.handleChange}
-                            indicatorColor={"primary"}
-                            textColor={"primary"}
-                            aria-label={"import tabs"}
-                            centered={true}>
-
-                            <Tab label={"Import data"} {...this.setTabProps(0)} />
-                            <Tab label={"Import rules"} {...this.setTabProps(1)} />
-                        </Tabs>
-                    </Paper>
-                    <TabPanel value={this.state.value} index={0} >
-                        <TabBody textValue={"Select data file"}>
-                            Data
-                        </TabBody>
-                    </TabPanel>
-                    <TabPanel value={this.state.value} index={1} >
-                        <TabBody textValue={"Select rules file"}>
-                            Rules
-                        </TabBody>
-                    </TabPanel>
+                <Paper square={true} className={"import-panel"}>
+                    <img src={"https://miro.medium.com/max/12000/1*U6BGoYgGAqEElQdCR_zEtA.jpeg"} alt={"empty"}/>
                 </Paper>
             </Box>
         );
