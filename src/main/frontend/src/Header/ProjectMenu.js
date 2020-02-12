89ce2ced41a3cbe4f4a0341d82eeba65ff9f9c9b
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-02-12 10:13:18 +0100

    Delete add-project button from project-menu

--- src/main/frontend/src/Header/ProjectMenu.js
+++ src/main/frontend/src/Header/ProjectMenu.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
+import Box from"@material-ui/core/Box";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import IconButton from "@material-ui/core/IconButton";
-import Icon from "@material-ui/core/Icon";
-import "./ProjectMenu.css";
 
 class ProjectMenu extends Component {
     constructor(props) {
@@ -50,39 +48,32 @@ class ProjectMenu extends Component {
         }
 
         return (
-            <div className={"project-menu"}>
-                <div className={"project-list"}>
-                    <List component={"nav"} >
-                        <ListItem
-                            button
-                            aria-haspopup={"true"}
-                            aria-controls={"lock-menu"}
-                            onClick={this.handleClickListItem}>
-                            <ListItemText primary={"Active project: " + value.name}/>
-                        </ListItem>
-                    </List>
-                    <Menu
-                        anchorEl={this.state.anchorE1}
-                        keepMounted
-                        open={Boolean(this.state.anchorE1)}
-                        onClose={this.handleClose}>
-                        {this.props.projects.map((option, index) => (
-                            <MenuItem
-                                key={option.name}
-                                disabled={index === 0}
-                                selected={index === this.state.selectedIndex}
-                                onClick={event => this.handleMenuItemClick(event, index)}>
-                                {option.name}
-                            </MenuItem>
-                        ))}
-                    </Menu>
-                </div>
-                <div>
-                    <IconButton>
-                        <Icon>add-circle</Icon>
-                    </IconButton>
-                </div>
-            </div>
+            <Box component={"div"} flexGrow={1}>
+                <List component={"nav"} >
+                    <ListItem
+                        button
+                        aria-haspopup={"true"}
+                        aria-controls={"lock-menu"}
+                        onClick={this.handleClickListItem}>
+                        <ListItemText primary={"Active project: " + value.name}/>
+                    </ListItem>
+                </List>
+                <Menu
+                    anchorEl={this.state.anchorE1}
+                    keepMounted
+                    open={Boolean(this.state.anchorE1)}
+                    onClose={this.handleClose}>
+                    {this.props.projects.map((option, index) => (
+                        <MenuItem
+                            key={option.name}
+                            disabled={index === 0}
+                            selected={index === this.state.selectedIndex}
+                            onClick={event => this.handleMenuItemClick(event, index)}>
+                            {option.name}
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </Box>
         )
     }
 }
