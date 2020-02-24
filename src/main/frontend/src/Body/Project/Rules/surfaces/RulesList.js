6fc10db2b75dc82316e14f78cae5e8a0e0668337
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-02-24 19:02:55 +0100

    Fix problems with display and change files name

--- src/main/frontend/src/Body/Project/Rules/surfaces/RulesList.js
+++ src/main/frontend/src/Body/Project/Rules/surfaces/RulesList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
-import "./RuleList.css";
+import "./RulesList.css";
 
 function RulesList(props) {
     const children = props.children;
