6813bcc7c7dab81d24fced901f7fd13b636d44b1
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-03-11 18:32:25 +0100

    Add basic upper bar in CrossValidation tab

--- src/main/frontend/src/Body/Project/CrossValidation/CrossValidation.js
+++ src/main/frontend/src/Body/Project/CrossValidation/CrossValidation.js
@@ -1,16 +1,23 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import PropTypes from "prop-types";
 import RuleWorkBox from "../../../RuleWorkComponents/Containers/RuleWorkBox";
-import RuleWorkButton from "../../../RuleWorkComponents/Inputs/RuleWorkButton";
+import RuleWorkDrawer from "../../../RuleWorkComponents/Containers/RuleWorkDrawer";
 import RuleWorkList from "../../../RuleWorkComponents/DataDisplay/RuleWorkList";
 import RuleWorkSelect from "../../../RuleWorkComponents/Inputs/RuleWorkSelect";
+import RuleWorkSmallBox from "../../../RuleWorkComponents/Containers/RuleWorkSmallBox";
 import RuleWorkSnackbar from "../../../RuleWorkComponents/Feedback/RuleWorkSnackbar";
+import RuleWorkTextField from "../../../RuleWorkComponents/Inputs/RuleWorkTextField";
+import RuleWorkTooltip from "../../../RuleWorkComponents/Inputs/RuleWorkTooltip";
 import StyledButton from "../../../RuleWorkComponents/Inputs/StyledButton";
 import StyledCircularProgress from "../../../RuleWorkComponents/Feedback/StyledCircularProgress";
+import StyledDivider from "../../../RuleWorkComponents/DataDisplay/StyledDivider";
 import StyledPaper from "../../../RuleWorkComponents/Surfaces/StyledPaper";
-import Divider from "@material-ui/core/Divider";
-import Cog from "mdi-material-ui/Cogs";
-import "./CrossValidation.css"
+import StyledToggleButton from "../../../RuleWorkComponents/Inputs/StyledToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import Calculator from "mdi-material-ui/Calculator";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import {mdiCog} from "@mdi/js";
 
 class CrossValidation extends Component {
     constructor(props) {
@@ -20,25 +27,45 @@ class CrossValidation extends Component {
             loading: false,
             data: [],
             displayedData: [],
+            display: 0,
+            foldNumber: 1,
             foldIndex: 0,
             folds: [],
             openSettings: false,
             snackbarProps: undefined,
         };
+
+        this.upperBar = React.createRef();
     }
 
     componentDidMount() {
         console.log("Fetching cross-validation from server...");
     }
 
-    onCalculateClick = () => {
-        console.log("Calculating cross-validation on server...");
-    };
-
     onSettingsClick = () => {
         this.setState(prevState => ({
             openSettings: !prevState.openSettings,
         }));
+    };
+
+    onSettingsClose = () => {
+        this.setState({
+            openSettings: false,
+        });
+    };
+
+    onFoldNumberChange = (event) => {
+        const input = event.target.value;
+
+        if (!isNaN(input)) {
+            this.setState({
+                foldNumber: Number(input),
+            });
+        }
+    };
+
+    onCalculateClick = () => {
+        console.log("Calculating cross-validation on server...");
     };
 
     onFoldChange = (event) => {
@@ -47,7 +74,15 @@ class CrossValidation extends Component {
         })
     };
 
-    onSnackbarProps = (event, reason) => {
+    onDisplayChange = (event, newDisplay) => {
+        if (typeof newDisplay !== 'number') return;
+
+        this.setState({
+            display: newDisplay,
+        });
+    };
+
+    onSnackbarClose = (event, reason) => {
         if (reason === 'clickaway') {
             return;
         }
@@ -57,21 +92,12 @@ class CrossValidation extends Component {
         });
     };
 
-    render() {
-        const {loading, displayedData, foldIndex, folds, snackbarProps} = this.state;
+    renderResultsActions = () => {
+        const {display, folds, foldIndex} = this.state;
 
-        return (
-            <RuleWorkBox id={"rule-work-cross-validation"} styleVariant={"tab"}>
-                <StyledPaper id={"cross-validation-bar"} styleVariant={"bar"} square={true} variant={"outlined"}>
-                    <RuleWorkButton
-                        ariaLabel={"toggle-settings-button"}
-                        buttonVariant={"icon"}
-                        onClick={this.onSettingsClick}
-                        title={"Click to customize other settings"}
-                    >
-                        <Cog />
-                    </RuleWorkButton>
-                    <Divider flexItem={true} orientation={"vertical"} />
+        if (Array.isArray(folds) && folds.length) {
+            return (
+                <Fragment>
                     <RuleWorkSelect
                         label={"Choose fold"}
                         onChange={this.onFoldChange}
@@ -79,16 +105,94 @@ class CrossValidation extends Component {
                     >
                         {folds}
                     </RuleWorkSelect>
-                    <span style={{flexGrow: 1}} />
-                    <StyledButton
-                        buttonVariant={"contained"}
-                        onClick={this.onCalculateClick}
-                        styleVariant={"green"}
+                    <StyledDivider />
+                    <ToggleButtonGroup
+                        aria-label={"display-toggle-button-group"}
+                        exclusive={true}
+                        onChange={this.onDisplayChange}
+                        value={display}
                     >
-                        Calculate
-                    </StyledButton>
+                        <StyledToggleButton value={0}>
+                            Details of training set
+                        </StyledToggleButton>
+                        <StyledToggleButton value={1}>
+                            Error matrix
+                        </StyledToggleButton>
+                        <StyledToggleButton value={2}>
+                            Mean error matrix
+                        </StyledToggleButton>
+                    </ToggleButtonGroup>
+                </Fragment>
+            )
+        } else {
+            return null;
+        }
+    };
+
+    render() {
+        const {loading, displayedData, foldNumber, openSettings, snackbarProps} = this.state;
+
+        return (
+            <RuleWorkBox id={"rule-work-cross-validation"} styleVariant={"tab"}>
+                <StyledPaper
+                    id={"cross-validation-bar"}
+                    paperRef={this.upperBar}
+                    styleVariant={"bar"}
+                    square={true}
+                    variant={"outlined"}
+                >
+                    <RuleWorkTooltip title={"Click to customize number of folds"}>
+                        <StyledButton
+                            aria-label={"cross-validation-settings-button"}
+                            isIcon={true}
+                            onClick={this.onSettingsClick}
+                        >
+                            <SvgIcon><path d={mdiCog} /></SvgIcon>
+                        </StyledButton>
+                    </RuleWorkTooltip>
+                    <StyledDivider />
+                    <RuleWorkTooltip title={`Current number of folds: ${foldNumber}`}>
+                        <StyledButton
+                            aria-label={"cross-validation-calculate"}
+                            disable={!this.props.project & loading}
+                            disableElevation={true}
+                            onClick={this.onCalculateClick}
+                            startIcon={<Calculator />}
+                            themeVariant={"primary"}
+                            variant={"contained"}
+                        >
+                            Calculate
+                        </StyledButton>
+                    </RuleWorkTooltip>
+                    <span style={{flexGrow: 1}} />
+                    {this.renderResultsActions()}
                 </StyledPaper>
-                <RuleWorkBox id={"cross-validation-body"} styleVariant={"tab-body1"}>
+                <RuleWorkDrawer
+                    height={this.upperBar.current ? this.upperBar.current.offsetHeight : undefined}
+                    id={"cross-validation-settings-drawer"}
+                    open={openSettings}
+                >
+                    <StyledDivider orientation={"horizontal"} styleVariant={"panel"} />
+                    <RuleWorkSmallBox id={"fold-number-selector"} >
+                        <RuleWorkTextField
+                            value={foldNumber}
+                            onChange={this.onFoldNumberChange}
+                            style={{maxWidth: 128}}
+                        >
+                            Choose number of folds
+                        </RuleWorkTextField>
+                    </RuleWorkSmallBox>
+                    <RuleWorkSmallBox id={"cross-validation-footer"} styleVariant={"footer"}>
+                        <StyledButton
+                            isIcon={true}
+                            onClick={this.onSettingsClose}
+                            themeVariant={"secondary"}
+                        >
+                            <ChevronLeftIcon />
+                        </StyledButton>
+                    </RuleWorkSmallBox>
+                </RuleWorkDrawer>
+                <RuleWorkBox id={"cross-validation-body"} styleVariant={"tab-body"}>
                     {loading ?
                         <StyledCircularProgress />
                         :
@@ -97,7 +201,7 @@ class CrossValidation extends Component {
                         </RuleWorkList>
                     }
                 </RuleWorkBox>
-                <RuleWorkSnackbar {...snackbarProps} onClose={this.onSnackbarProps} />
+                <RuleWorkSnackbar {...snackbarProps} onClose={this.onSnackbarClose} />
             </RuleWorkBox>
         )
     }
