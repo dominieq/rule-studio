9a9fd62203eabedf890a38732b06ef0ec165c302
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-03-21 20:38:53 +0100

    Make utils more customisable

--- src/main/frontend/src/RuleWorkComponents/DataDisplay/StyledFileChip.js
+++ src/main/frontend/src/RuleWorkComponents/DataDisplay/StyledFileChip.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {makeStyles} from "@material-ui/core/styles";
 import Chip from "@material-ui/core/Chip";
 
@@ -19,23 +20,32 @@ const useStyles = makeStyles(theme => ({
             backgroundColor: theme.palette.chip.background,
         }
     }
-}));
+}), {name: "MuiFileChip"});
 
 
 function StyledFileChip(props) {
-    const {...other} = props;
-    const classes = useStyles();
+    const {classes: propsClasses, ...other} = props;
+    const classes = {...useStyles(), ...propsClasses};
 
     return (
-        <Chip
-            {...other}
-            classes={{
-                root: classes.root,
-                clickable: classes.clickable,
-                deletable: classes.deletable,
-            }}
-        />
+        <Chip classes={classes} {...other} />
     );
 }
+
+StyledFileChip.propTypes =  {
+    avatar: PropTypes.element,
+    classes: PropTypes.object,
+    className: PropTypes.string,
+    clickable: PropTypes.bool,
+    color: PropTypes.oneOf(["default", "primary", "secondary"]),
+    component: PropTypes.elementType,
+    deleteIcon: PropTypes.element,
+    disabled: PropTypes.bool,
+    icon: PropTypes.element,
+    label: PropTypes.node,
+    onDelete: PropTypes.func,
+    size: PropTypes.oneOf(["small", "medium"]),
+    variant: PropTypes.oneOf(["default", "outlined"])
+};
 
 export default StyledFileChip;
