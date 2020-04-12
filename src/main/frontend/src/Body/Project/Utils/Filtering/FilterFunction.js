3a4f24e155cb4375d38b3f2459a313708cd1a7d1
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-04-12 18:22:13 +0200

    Improve filtering a little bit

--- src/main/frontend/src/Body/Project/Utils/Filtering/FilterFunction.js
+++ src/main/frontend/src/Body/Project/Utils/Filtering/FilterFunction.js
@@ -2,11 +2,21 @@ const filterFunction = (filterText, items) => {
     if (filterText === "") {
         return items;
     } else {
+        const filters = filterText.toString().toLowerCase().split(" ");
+
         let displayedItems = [];
         for (let i = 0; i < items.length; i++) {
             const object = {...items[i]};
+            let objectName = object.name.toString().toLowerCase();
 
-            if (object.name.toString().includes(filterText)) {
+            let matchName, matchTraits = true;
+            for (let j = 0; j < filters.length; j++) {
+                if (!objectName.includes(filters[j])) {
+                    matchName = false;
+                    break
+                }
+            }
+            if (matchName || matchTraits) {
                 displayedItems = [...displayedItems, object];
             }
         }
