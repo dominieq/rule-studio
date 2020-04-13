3a2997433c36f589a2c8b6d9884e6ea8e246e4e0
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-04-13 15:49:27 +0200

    Create RuleTable element

--- src/main/frontend/src/RuleWorkComponents/Feedback/RuleWorkDialog/Variants/CrossValidationDialog.js
+++ src/main/frontend/src/RuleWorkComponents/Feedback/RuleWorkDialog/Variants/CrossValidationDialog.js
@@ -1,18 +1,35 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, Fragment } from "react";
 import PropTypes from "prop-types";
 import ColouredTitle from "../../../DataDisplay/ColouredTitle";
 import RuleWorkDialog from "../RuleWorkDialog";
 import ObjectTable from "../Elements/ObjectTable";
 import TableItemsList from "../Elements/TableItemsList";
 import TraitsTable from "../Elements/TraitsTable";
+import RuleTable, { estimateTableHeight } from "../Elements/RuleTable";
 
 class CrossValidationDialog extends PureComponent {
     constructor(props) {
         super(props);
 
         this.state = {
-            itemInTableIndex: undefined
+            itemInTableIndex: undefined,
+            ruleTableHeight: 0
         };
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const { itemInTableIndex } = this.state;
+
+        if (!Number.isNaN(Number(itemInTableIndex))) {
+            const { ruleSet } = this.props;
+            let height = estimateTableHeight(ruleSet[itemInTableIndex].rule);
+
+            if (prevState.ruleTableHeight !== height) {
+                this.setState({
+                    ruleTableHeight: height
+                });
+            }
+        }
     }
 
     onExited = () => {
@@ -41,25 +58,29 @@ class CrossValidationDialog extends PureComponent {
     }
 
     render() {
-        const { itemInTableIndex } = this.state;
+        const { itemInTableIndex, ruleTableHeight } = this.state;
         const { item, ruleSet, ...other } = this.props;
         const { attributes, objects, suggestedDecision } = item.traits;
 
         return (
             <RuleWorkDialog
+                onEntered={this.onEntered}
                 onExited={this.onExited}
                 optional={"Suggested decision: " + suggestedDecision}
                 title={this.getCrossValidationTitle()}
                 {...other}
             >
-                <div id={"cross-validation-item-details"}>
+                <div id={"cross-validation-item-details"} style={{width: "40%"}}>
                     <ObjectTable
                         informationTable={{attributes, objects}}
                         objectIndex={item.id}
                         objectHeader={item.name.toString()}
                     />
                 </div>
-                <div id={"cross-validation-rules-table"} style={{display: "flex", flexDirection: "column"}}>
+                <div
+                    id={"cross-validation-rules-table"}
+                    style={{display: "flex", flexDirection: "column", width: "15%"}}
+                >
                     <TableItemsList
                         headerText={"Indices of covering rules"}
                         itemIndex={itemInTableIndex}
@@ -68,11 +89,19 @@ class CrossValidationDialog extends PureComponent {
                         table={item.tables.indicesOfCoveringRules}
                     />
                 </div>
-                <div id={"cross-validation-rule-characteristics"}>
+                <div
+                    id={"cross-validation-rule-characteristics"}
+                    style={{display: "flex", flexDirection: "column", width: "40%"}}
+                >
                     {!Number.isNaN(Number(itemInTableIndex)) &&
-                        <TraitsTable
-                            traits={ruleSet[itemInTableIndex].ruleCharacteristics}
-                        />
+                        <Fragment>
+                            <div id={"rule-table"} style={{marginBottom: "5%", minHeight: ruleTableHeight}}>
+                                <RuleTable rule={ruleSet[itemInTableIndex].rule} />
+                            </div>
+                            <div id={"traits-table"} style={{flexGrow: 1}}>
+                                <TraitsTable traits={ruleSet[itemInTableIndex].ruleCharacteristics} />
+                            </div>
+                        </Fragment>
                     }
                 </div>
             </RuleWorkDialog>
