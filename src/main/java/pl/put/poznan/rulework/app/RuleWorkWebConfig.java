ccdb0e5a514a9d8144cd2403fe0cf11f70274a00
tomdziwood <tomdziwood@gmail.com>
2020-04-21 10:08:52 +0200

    Make available to download rules in simple txt format
    Earlier downloading rules is modified to handle downloading in txt format. Now, request of download need additional parameter, which chooses format of file. To see how to use it, go to frontend/src/Demo/DemoRules/Donload.js.

--- src/main/java/pl/put/poznan/rulework/app/RuleWorkWebConfig.java
+++ src/main/java/pl/put/poznan/rulework/app/RuleWorkWebConfig.java
@@ -3,10 +3,7 @@ package pl.put.poznan.rulework.app;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import pl.put.poznan.rulework.enums.converters.StringToClassifierTypeConverter;
-import pl.put.poznan.rulework.enums.converters.StringToDefaultClassificationResultType;
-import pl.put.poznan.rulework.enums.converters.StringToRuleTypeConverter;
-import pl.put.poznan.rulework.enums.converters.StringToUnionTypeConverter;
+import pl.put.poznan.rulework.enums.converters.*;
 
 @Configuration
 public class RuleWorkWebConfig implements WebMvcConfigurer {
@@ -17,5 +14,6 @@ public class RuleWorkWebConfig implements WebMvcConfigurer {
         registry.addConverter(new StringToClassifierTypeConverter());
         registry.addConverter(new StringToDefaultClassificationResultType());
         registry.addConverter(new StringToRuleTypeConverter());
+        registry.addConverter(new StringToRulesFormatConverter());
     }
 }
