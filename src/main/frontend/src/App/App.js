4051a30ce1a51e2d80a2bf8f730229c09f285bd6
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-04-22 19:50:57 +0200

    Clean Header and ProjectMenu

--- src/main/frontend/src/App/App.js
+++ src/main/frontend/src/App/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { fetchProject, fetchProjects } from "./fetchFunctions";
 import Header from "../Header/Header";
-import ProjectMenu from "../Header/ProjectMenu";
+import { ProjectMenu } from "../Header/Elements";
 import Help from '../Body/Help/Help';
 import Home from "../Body/Home/Home";
 import Import from "../Body/Import/Import";
