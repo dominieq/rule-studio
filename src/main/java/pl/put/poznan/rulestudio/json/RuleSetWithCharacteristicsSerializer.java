62ce65ca29fb060c8d538f8961f5643600368568
tomdziwood <tomdziwood@gmail.com>
2020-05-09 11:03:46 +0200

    Serialize supporting objects of given ComputableRuleCharacteristic
    There is true-false array "isSupportingObject" next to "indicesOfCoveredObjects" array. New array tells, if given object from "indicesOfCoveredObjects" is also supporting object.

--- src/main/java/pl/put/poznan/rulestudio/json/RuleSetWithCharacteristicsSerializer.java
+++ src/main/java/pl/put/poznan/rulestudio/json/RuleSetWithCharacteristicsSerializer.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.ints.IntSet;
 import org.rulelearn.rules.ComputableRuleCharacteristics;
 import org.rulelearn.rules.RuleCharacteristics;
+import org.rulelearn.rules.RuleCoverageInformation;
 import org.rulelearn.rules.RuleSetWithCharacteristics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,11 +41,26 @@ public class RuleSetWithCharacteristicsSerializer extends JsonSerializer<RuleSet
             jsonGenerator.writeRawValue(mapper.writeValueAsString(ruleCharacteristics));
 
             if(ruleCharacteristics instanceof ComputableRuleCharacteristics) {
+                RuleCoverageInformation ruleCoverageInformation = ((ComputableRuleCharacteristics) ruleCharacteristics).getRuleCoverageInformation();
+                IntList indicesOfCoveredObjects = ruleCoverageInformation.getIndicesOfCoveredObjects();
+                IntSet indicesOfCoveredNotSupportingObjects = ruleCoverageInformation.getIndicesOfCoveredNotSupportingObjects();
+
                 jsonGenerator.writeFieldName("indicesOfCoveredObjects");
-                jsonGenerator.writeRawValue(
-                        mapper.writeValueAsString(
-                                ((ComputableRuleCharacteristics) ruleCharacteristics).getRuleCoverageInformation().getIndicesOfCoveredObjects()
-                        ));
+                jsonGenerator.writeRawValue(mapper.writeValueAsString(indicesOfCoveredObjects));
+
+
+                int numberOfCoveredObjects = indicesOfCoveredObjects.size();
+                boolean[] isSupportingObject = new boolean[numberOfCoveredObjects];
+                for(int j = 0; j < numberOfCoveredObjects; j++) {
+                    if(indicesOfCoveredNotSupportingObjects.contains( indicesOfCoveredObjects.getInt(j) )) {
+                        isSupportingObject[j] = false;
+                    } else {
+                        isSupportingObject[j] = true;
+                    }
+                }
+
+                jsonGenerator.writeFieldName("isSupportingObject");
+                jsonGenerator.writeRawValue(mapper.writeValueAsString(isSupportingObject));
             }
 
             jsonGenerator.writeEndObject();
