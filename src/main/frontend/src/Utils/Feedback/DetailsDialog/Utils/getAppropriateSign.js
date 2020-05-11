c5ebe2dcf04250ed8431e7ddfe7a923794a20af8
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-05-11 11:23:34 +0200

    Fix: Treat active field from attribute as bool

--- src/main/frontend/src/Utils/Feedback/DetailsDialog/Utils/getAppropriateSign.js
+++ src/main/frontend/src/Utils/Feedback/DetailsDialog/Utils/getAppropriateSign.js
@@ -1,18 +1,21 @@
 const getAppropriateSign = (object1, object2, attribute, tableIndex) => {
-    if(attribute.type === "description" || Object.keys(attribute).includes("identifierType") || attribute.active === "false") {
+    if (attribute.type === "description" || Object.keys(attribute).includes("identifierType") || !attribute.active) {
         return "--";
     } else {
-        if(object1[attribute.name] === object2[attribute.name] || object1[attribute.name] === "?" || object2[attribute.name] === "?") return "=";
+        if (object1[attribute.name] === object2[attribute.name] || object1[attribute.name] === "?" || object2[attribute.name] === "?") {
+            return "=";
+        }
 
-        if(attribute.type !== "decision") {
+        if (attribute.type !== "decision") {
             if(tableIndex === 0 || tableIndex === 2) return "\u227A";
             else return "\u227B";
         } else {
             switch(attribute.preferenceType) {
                 case "gain":
-                    if(attribute.valueType === "enumeration") {
+                    if (attribute.valueType === "enumeration") {
                         const idx1 = attribute.domain.indexOf(object1[attribute.name]);
                         const idx2 = attribute.domain.indexOf(object2[attribute.name]);
+
                         if(idx1 > idx2) return "\u227B";
                         else return "\u227A";
                     } else {
@@ -20,13 +23,14 @@ const getAppropriateSign = (object1, object2, attribute, tableIndex) => {
                         else return "\u227A";
                     }
                 case "cost":
-                    if(attribute.valueType === "enumeration") {
+                    if (attribute.valueType === "enumeration") {
                         const idx1 = attribute.domain.indexOf(object1[attribute.name]);
                         const idx2 = attribute.domain.indexOf(object2[attribute.name]);
+
                         if(idx1 < idx2) return "\u227B";
                         else return "\u227A";
                     } else {
-                        if(Number(object1[attribute.name]) < Number(object2[attribute.name])) return "\u227B";                         
+                        if (Number(object1[attribute.name]) < Number(object2[attribute.name])) return "\u227B";
                         else return "\u227A";
                     }
                 case "none":
