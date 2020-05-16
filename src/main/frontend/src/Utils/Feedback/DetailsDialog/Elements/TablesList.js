3122e6eca72afbe4c993d718e554d32a4fe18095
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-05-16 13:25:53 +0200

    Delete an array icon; fixes #58

--- src/main/frontend/src/Utils/Feedback/DetailsDialog/Elements/TablesList.js
+++ src/main/frontend/src/Utils/Feedback/DetailsDialog/Elements/TablesList.js
@@ -4,11 +4,9 @@ import clsx from "clsx";
 import { makeStyles } from "@material-ui/core";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import Typography from "@material-ui/core/Typography";
-import CodeBrackets from "mdi-material-ui/CodeBrackets";
 
 const listStyles = makeStyles(theme => ({
     root: {
@@ -84,7 +82,6 @@ function TablesList(props) {
                     onClick={() => onTableSelected(item.id)}
                     selected={tableIndex === item.id}
                 >
-                    <ListItemIcon><CodeBrackets /></ListItemIcon>
                     <ListItemText
                         className={listClasses.multiline}
                         primary={item.name}
