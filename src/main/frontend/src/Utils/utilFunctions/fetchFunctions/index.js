d1961b427eb2cc7713d279f2aab89c2bdeb694b4
Dominik <dominik.szmyt@student.put.poznan.pl>
2020-10-13 21:09:19 +0200

    Move export/importProject to fetchProject FIX

--- src/main/frontend/src/Utils/utilFunctions/fetchFunctions/index.js
+++ src/main/frontend/src/Utils/utilFunctions/fetchFunctions/index.js
@@ -1,6 +1,6 @@
 export {default as downloadMatrix} from "./downloadMatrix";
 export {default as downloadRules} from "./downloadRules";
-export {default as exportProject} from "./project/exportProject";
+export {default as exportProject} from "./fetchProject/exportProject";
 export {default as fetchClassification} from "./fetchClassification";
 export {default as fetchCones} from "./fetchCones/fetchCones";
 export {default as fetchConeObjects} from "./fetchCones/fetchConeObjects";
@@ -11,5 +11,5 @@ export {default as fetchObjectsComparison} from "./fetchObjects/fetchObjectsComp
 export {default as fetchRules} from "./fetchRules";
 export {default as fetchUnion} from "./fetchUnions/fetchUnion";
 export {default as fetchUnions} from "./fetchUnions/fetchUnions";
-export {default as importProject} from "./project/importProject";
+export {default as importProject} from "./fetchProject/importProject";
 export {default as uploadRules} from "./uploadRules";
