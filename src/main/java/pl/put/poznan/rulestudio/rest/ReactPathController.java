05b71730cedf79ebcbe443b03a7158778b7578dd
emsi-iggy <michaelcoldpl@gmail.com>
2020-10-17 16:24:12 +0200

    Catch tabs paths in spring
    Gave up on /error path, because it caught exceptions threw by the classes, e.g. WrongParameterException in the MetaDataService class. Those exceptions couldn't be forwarded.

---
+++ src/main/java/pl/put/poznan/rulestudio/rest/ReactPathController.java
@@ -0,0 +1,22 @@
+package pl.put.poznan.rulestudio.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RequestMapping(value = "/{id}")
+@Controller
+public class ReactPathController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DominanceConesController.class);
+
+    @RequestMapping(value = {"/data","/cones","/unions","/rules","/classification","/crossvalidation"})
+    public String returnIndex(
+            @PathVariable("id") String id) {
+        logger.info("Getting appropriate tab for project with id = " + id);
+
+        return "../index.html";
+    }
+}
